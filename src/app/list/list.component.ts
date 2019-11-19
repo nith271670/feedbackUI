57fa0949b6670dd6f30f33c9c562427e9ece2e87
nimisha thilak <nimisha.thilak@elektrobit.com>
2019-11-19 11:44:36 +0530

    phase 2 docs

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -49,10 +49,11 @@ export class ListComponent implements OnInit {
 
 
   exportAsXLSX():void {
-    this.httpService.getList().subscribe(response => {
-      // console.log(response);
-      this.feedBackList = response as [];
-    });
+
+    // this.httpService.getList().subscribe(response => {
+    //    console.log(response);
+    //   this.feedBackList = response as [];
+    // });
 
 
     if(this.selectedValue !== undefined){
